83f47166f06f2ead9e4af3fcf79cb910732fbb9a
JankElephant <jankelephant@gmail.com>
2018-04-10 06:47:35 -0500

    hide

--- betterclick.js
+++ betterclick.js
@@ -1,9 +1,12 @@
 function populate_checks() {
   // Checkmark what is found to be checked in local storage
   var boxes = document.querySelectorAll("div[class=item-box]");
-  for (var i = 0; i < boxes.length; i++){
-    if (localStorage.getItem(boxes[i].id) !== null){
+  for (var i = 0; i < boxes.length; i++) {
+    if (localStorage.getItem(boxes[i].id) !== null) {
       boxes[i].style.opacity = 0.2;
+      if (localStorage.getItem("hide_checked") !== null) {
+        boxes[i].style.display = "none";
+      }
     }
   }
 }
@@ -11,11 +14,35 @@ function populate_checks() {
 function mark_checked(item){
   // Store checkbox changes in local storage
   if (localStorage.getItem(item.id) === null) {
-    localStorage.setItem(item.id, "checked");
+    localStorage.setItem(item.id, true);
     item.style.opacity = 0.2;
+    if (localStorage.getItem("hide_checked") !== null) {
+      item.style.display = "none";
+    }
   } else {
     localStorage.removeItem(item.id);
     item.style.opacity = 1;
   }
 }
+
+function toggle_hide(){
+  // Toggle hiding of checked items
+  var boxes = document.querySelectorAll("div[class=item-box]");
+  if (localStorage.getItem("hide_checked") === null) {
+    localStorage.setItem("hide_checked", true);
+    for (var i = 0; i < boxes.length; i++) {
+      if (localStorage.getItem(boxes[i].id) !== null) {
+        boxes[i].style.display = "none";
+      }
+    }
+  } else {
+    localStorage.removeItem("hide_checked");
+    for (var i = 0; i < boxes.length; i++) {
+      if (localStorage.getItem(boxes[i].id) !== null) {
+        boxes[i].style.display = "block";
+      }
+    }
+  }
+}
+
 populate_checks();
